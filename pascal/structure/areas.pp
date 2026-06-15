a527faec41f0759ef3b01c63f8e3a9db6f0ed735
zam-dot <sam.far.git@gmail.com>
2026-06-15 12:02:52 +0200

    structures

---
+++ pascal/structure/areas.pp
@@ -0,0 +1,25 @@
+unit Areas;
+
+{$mode objfpc}{$H+}{$J-}
+
+interface
+
+function CalcAreaSquare(side: real): real;
+function CalcAreaCircle(radius: real): real;
+
+implementation
+
+const
+  shortPI: real = 3.14;
+
+function CalcAreaSquare(side: real): real;
+begin
+  Result := side * side;
+end;
+
+function CalcAreaCircle(radius: real): real;
+begin
+  Result := shortPI * radius * radius;
+end;
+
+end.
